3f626761e74e9b95d80ddb4d9dd9ef9b8992788d
timpostuvan <tim.postuvan@gmail.com>
2021-04-22 18:36:22 +0200

    Add color distribution data to extracted features

--- src/preprocessing/extract_features.py
+++ src/preprocessing/extract_features.py
@@ -25,11 +25,9 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 from torch.optim import SGD, Adam
 
-#random.seed(1)
-#np.random.seed(1)
-#torch.manual_seed(1)
-
-
+random.seed(1)
+np.random.seed(1)
+torch.manual_seed(1)
 
 
 if __name__ == "__main__":
@@ -43,6 +41,7 @@ if __name__ == "__main__":
     # Feature extractor
     cutoff = 4
     embedding_size = 512
+    histogram_size = 10
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
@@ -63,7 +62,11 @@ if __name__ == "__main__":
     for file_name, label in zip(files, labels):
         CT_name = "/"+ file_name.strip('.nii.gz') + '.npy'
         file_path = path + CT_name
-        img = torch.tensor(np.load(file_path))
+        img = np.load(file_path)
+        color_data = img[img > 0].flatten()
+        color_distribution = np.histogram(color_data, bins=10, range=(0, 1))[0] / color_data.size
+
+        img = torch.tensor(img)
         img = img.repeat(1, 3, 1, 1)
 
         num_slices = img.shape[0]
@@ -77,8 +80,8 @@ if __name__ == "__main__":
         del img
         torch.cuda.empty_cache()
 
-
-        X.append(extracted_features.cpu().detach().numpy())           
+        features = np.append(extracted_features.cpu().detach().numpy(), color_distribution) 
+        X.append(features)           
         y.append(label)
 
     
@@ -94,7 +97,8 @@ if __name__ == "__main__":
     print(data_frame.head())
 
     embedded_images = pd.DataFrame(X)
-    embedded_images.columns = ["v" + str(i) for i in range(embedding_size)]
+    embedded_images.columns = (["v" + str(i) for i in range(embedding_size)] + 
+                               ["h" + str(i) for i in range(histogram_size)])
 
     data_frame = pd.concat([data_frame, embedded_images], axis=1)
     data_frame.to_csv("../../data/embedded-datasets/" + dataset + "_embedded_dataset.csv", index=False)
