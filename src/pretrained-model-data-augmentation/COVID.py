20a5dc6f5cf68845736be4f755ce3b0f37b1737f
timpostuvan <tim.postuvan@gmail.com>
2021-04-26 17:24:56 +0200

    Improve pretrained model with data augmentation

--- src/pretrained-model-data-augmentation/COVID.py
+++ src/pretrained-model-data-augmentation/COVID.py
@@ -4,8 +4,6 @@ import tqdm
 import copy
 import logging
 import matplotlib.pyplot as plt
-import monai
-from monai.transforms import RandGaussianNoise
 
 logging.basicConfig(level=logging.INFO)
 
@@ -18,7 +16,7 @@ def prepare_data (path_train):
 	for x in f:
 	    [filename, label] = x.split(",")
 	    label = int(label)
-	    if (label == 0):
+	    if(label == 0):
 	        negative_files.append(filename)
 	    else :
 	        positive_files.append(filename)
@@ -27,20 +25,26 @@ def prepare_data (path_train):
 
 	random.shuffle(positive_files)
 	plen = len(positive_files)
-	positive_files_train = positive_files[0:int(plen*0.7)]
-	positive_files_validation = positive_files[int(plen*0.7):int(plen*0.8)]
+	positive_files_train = positive_files[0:int(plen*0.65)]
+	positive_files_validation = positive_files[int(plen*0.65):int(plen*0.8)]
 	positive_files_test = positive_files[int(plen*0.8):plen]
 
 	random.shuffle(negative_files)
 	nlen = len(negative_files)
-	negative_files_train = negative_files[0:int(nlen*0.7)]
-	negative_files_validation = negative_files[int(nlen*0.7):int(nlen*0.8)]
+	negative_files_train = negative_files[0:int(nlen*0.65)]
+	negative_files_validation = negative_files[int(nlen*0.65):int(nlen*0.8)]
 	negative_files_test = negative_files[int(nlen*0.8):nlen]
 
 	data_augmentation = transforms.Compose([
-					        transforms.RandomRotation(degrees=90),
-					        transforms.RandomHorizontalFlip(),
-					        RandGaussianNoise(prob=0.1),
+							transforms.RandomApply(torch.nn.ModuleList([
+								transforms.RandomRotation(degrees=15)]), 
+							p=0.4),
+							transforms.RandomApply(torch.nn.ModuleList([
+								 transforms.RandomCrop(size=(200, 200)),
+								 transforms.Pad(padding=12, fill=0, padding_mode='constant')]), 
+							p=0.4),
+					        transforms.RandomHorizontalFlip(p=0.4),
+					        transforms.RandomVerticalFlip(p=0.4),
 					        transforms.Normalize(mean=[0.485, 0.456, 0.406],
 					                             std=[0.229, 0.224, 0.225]) 
 					    ])
@@ -54,13 +58,12 @@ def prepare_data (path_train):
 							  is_train=True,
 							  transforms=data_augmentation)
 
-
 	validation_dataset = MYDataset(path="../../data/processed/train-whole-sampled", 
 	    						   positive_files=positive_files_validation, 
 	    						   negative_files=negative_files_validation, 
 	    						   is_train=False,
 	    						   transforms=normalize)
-
+	
 	test_dataset = MYDataset(path="../../data/processed/train-whole-sampled", 
 						     positive_files=positive_files_test, 
 						     negative_files=negative_files_test, 
@@ -83,13 +86,14 @@ def train(model, dataloaders, optimizer, args, scheduler=None):
     val_scores = []
     test_scores = []
     for epoch in tqdm.tqdm(range(args['num_epochs'])):
-        for iter_i, (x, labels) in enumerate(dataloaders['train']):
+        for iter_i, (x, color, labels) in enumerate(dataloaders['train']):
             x = x.to(args['device'])
+            color = color.to(args['device'])
             labels = labels.to(args['device'])
 
             model.train()
             optimizer.zero_grad()
-            pred = model(x)
+            pred = model(x, color)
 
             loss = model.loss(pred, labels)
             loss.backward()
@@ -108,7 +112,6 @@ def train(model, dataloaders, optimizer, args, scheduler=None):
             val_max = scores['val']
             best_model = copy.deepcopy(model)
 
-
     log = 'Best, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
     scores, test_predictions = test(best_model, dataloaders, args)
     logging.info(log.format(scores['train'], scores['val'], scores['test']))
@@ -124,10 +127,12 @@ def test(model, dataloaders, args):
 		num_batches = 0
 		all_batches_labels = []
 		all_batches_pred = []
-		for (x, labels) in dataloader:
+		for (x, color, labels) in dataloader:
 		    x = x.to(args['device'])
+		    color = color.to(args['device'])
 		    label = labels.to(args['device'])
-		    pred = model(x)
+
+		    pred = model(x, color)
 		    all_batches_pred.extend(list(pred.flatten().data.cpu().numpy()))
 		    all_batches_labels.extend(list(labels.flatten().data.cpu().numpy()))
 
@@ -144,34 +149,38 @@ def test(model, dataloaders, args):
 
 
 if __name__ == "__main__":
-	# Set seeds for reproducability
-#	random.seed(1)
-#	np.random.seed(1)
-#	torch.manual_seed(1)
-#	monai.utils.set_determinism(seed=1, additional_settings=None)
 
-
-	dataloaders = prepare_data("../../data/train.txt")
-
-	# hyperparameters
-	learning_rate = 0.005
-	num_epochs = 40
-
-
-	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-	model = Network()
-	model = model.to(device)
-	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-	scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.005, max_lr=0.01, cycle_momentum=False)
-	arguments = {'device':device, 'num_epochs':num_epochs}
-
+	NUM_EXPERIMENTS = 3
+	for i in range(NUM_EXPERIMENTS):
+		
+		# Set seeds for reproducibility
+		random.seed(i)
+		np.random.seed(i)
+		torch.manual_seed(i)
 	
-	train_scores, val_scores, test_scores, test_predictions, best_model = train(model, dataloaders, optimizer, arguments, scheduler)
-	torch.save(best_model.state_dict(), "../models/pretrained_model_data_augmentation.pt")
+		dataloaders = prepare_data("../../data/train.txt")
 
-	epochs = list(range(len(train_scores)))
-	plt.plot(epochs, train_scores, 'b-', label="train")
-	plt.plot(epochs, val_scores, 'g-', label="validation")
-	plt.plot(epochs, test_scores, 'r-', label="test")
-	plt.legend()
-	plt.show()
+		# hyperparameters
+		learning_rate = 0.005
+		num_epochs = 20
+	
+
+		device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+		model = Network()
+		model = model.to(device)
+		optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+		scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.005, max_lr=0.01, cycle_momentum=False)
+		arguments = {'device':device, 'num_epochs':num_epochs}
+
+		
+		train_scores, val_scores, test_scores, test_predictions, best_model = train(model, dataloaders, optimizer, arguments, scheduler)
+		torch.save(best_model.state_dict(), f"../models/pretrained_model_data_augmentation_{i}.pt")
+
+		epochs = list(range(len(train_scores)))
+		plt.plot(epochs, train_scores, 'b-', label="train")
+		plt.plot(epochs, val_scores, 'g-', label="validation")
+		plt.plot(epochs, test_scores, 'r-', label="test")
+		plt.legend()
+		plt.savefig(f"graf_{i}")
+		plt.clf()
+	#	plt.show()
